c38211e8a4509f54b71beade8d4127ce33c6f01c
shubham <shubhampatil080699@gmail.com>
2024-03-30 12:18:30 +0530

    First commit

---
+++ EmployeeForm.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeForm.aspx.cs" Inherits="Maven.EmployeeForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Employee Form</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!-- Insert Form -->
+            <h2>Insert New Employee</h2>
+            <div>
+                <label for="txtEmpCode">Employee Code:</label>
+                <asp:TextBox ID="txtEmpCode" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label for="txtEname">Employee Name:</label>
+                <asp:TextBox ID="txtEname" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <label for="ddlDept">Department:</label>
+                <asp:DropDownList ID="ddlDept" runat="server"></asp:DropDownList>
+            </div>
+            <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+            <hr />
+
+            <!-- GridView to Display Employee Information -->
+            <h2>Employee Information</h2>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowEditing="GridView1_RowEditing" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting">
+                <Columns>
+                    <asp:BoundField DataField="EmpCode" HeaderText="Employee Code" />
+                    <asp:TemplateField HeaderText="Employee Name">
+                        <ItemTemplate>
+                            <asp:Label ID="lblEname" runat="server" Text='<%# Bind("Ename") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtEditEname" runat="server" Text='<%# Bind("Ename") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="DeptName" HeaderText="Department" />
+                    <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EmployeeForm.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace Maven
+{
+    public partial class EmployeeForm : System.Web.UI.Page
+    {
+        // Connection string to your database
+        string connectionString = @"Data Source=LAPTOP-QNOKS7JJ\SQLEXPRESS01;Initial Catalog=Task;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+                PopulateDepartments();
+            }
+        }
+
+        protected void BindGridView()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT e.EmpId, e.EmpCode, e.Ename, d.DeptName " +
+                               "FROM Employee e INNER JOIN Department d ON e.DeptId = d.DeptId";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                GridView1.DataSource = reader;
+                GridView1.DataBind();
+
+                reader.Close();
+            }
+        }
+
+        protected void PopulateDepartments()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT DeptId, DeptName FROM Department";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                ddlDept.DataSource = reader;
+                ddlDept.DataTextField = "DeptName";
+                ddlDept.DataValueField = "DeptId";
+                ddlDept.DataBind();
+
+                ddlDept.Items.Insert(0, new ListItem("--Select Department--", "0"));
+
+                reader.Close();
+            }
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string empCode = txtEmpCode.Text;
+            string empName = txtEname.Text;
+            int deptId = Convert.ToInt32(ddlDept.SelectedValue);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Employee (EmpCode, Ename, DeptId) VALUES (@EmpCode, @Ename, @DeptId)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@EmpCode", empCode);
+                command.Parameters.AddWithValue("@Ename", empName);
+                command.Parameters.AddWithValue("@DeptId", deptId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            BindGridView();
+            ClearInsertFields();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int empId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values["EmpId"]);
+            string newName = ((TextBox)row.FindControl("txtEditEname")).Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Employee SET Ename = @NewName WHERE EmpId = @EmpId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@NewName", newName);
+                command.Parameters.AddWithValue("@EmpId", empId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int empId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values["EmpId"]);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Employee WHERE EmpId = @EmpId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@EmpId", empId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            BindGridView();
+        }
+
+        protected void ClearInsertFields()
+        {
+            txtEmpCode.Text = "";
+            txtEname.Text = "";
+            ddlDept.SelectedIndex = 0;
+        }
+    }
+}
